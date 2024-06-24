8b37c0e7101c3535c10c62a12cdc47277f478e7e
ElDoceIT <it@eldoce.com.ar>
2024-06-23 21:28:42 -0300

    primera etapa con el micro usuario cargado, faltan las consulta a BD, viendo si uso mysql o post todo sobre docker.

---
+++ msvc.usuario/src/main/java/org/leo/msvc/usuario/msvc/usuario/repositories/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package org.leo.msvc.usuario.msvc.usuario.repositories;
+
+import org.leo.msvc.usuario.msvc.usuario.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+}
---
+++ msvc.usuario/src/main/java/org/leo/msvc/usuario/msvc/usuario/repositories/UsuarioService.java
@@ -0,0 +1,9 @@
+package org.leo.msvc.usuario.msvc.usuario.repositories;
+
+import org.leo.msvc.usuario.msvc.usuario.entity.Usuario;
+
+public class UsuarioService {
+
+    public Usuario buscarId(Long id);
+
+}
